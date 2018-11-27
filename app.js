f3be7f305788bdf80267d0583753dbfb8c1abb6f
mastanciu <mastanciu@bitdefender.com>
2018-11-27 17:30:16 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express(); //express static
+hbs.registerPartials(__dirname + '/views/partials'); //partial views (include)
+app.set('view engine', 'hbs'); //set configurations
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server log!');
+        }
+    });
+    console.log(log);
+    next();
+}); //register middleware
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs'); //maintenance middleware
+// });
+
+app.use(express.static(__dirname + '/public')); //register static middleware
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home',
+        welcomeMessage: 'Welcome!'
+    })
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Error'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000!');
+});
